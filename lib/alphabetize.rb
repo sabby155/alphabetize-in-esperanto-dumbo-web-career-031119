d450e7e5656289b2b3ea8d9fa7a655a258fd6e39
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-11 00:43:38 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -43,9 +43,8 @@ new_hash = return_all_keys(new_hash)
 end
 
 
- def alphabetize(arr)	def alphabetize(arr)
-  # code here	
- end 	alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ 	def alphabetize(arr)
+ 	alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
  a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
 word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
